f6d5e5c59817d787d58e324a35684335ae29427f
Riouallon Vincent <riouallonvincent@gmail.com>
2014-10-17 08:35:57 +0200

    archi_rentree

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" >
+	<head>
+		<title>  </title>
+
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="author" 			content="Vincent Riouallon - Gilles Biannic" />
+		<meta name="description" 		content="site derenytree de l'isen" />
+
+	    <link href="views/css/style.css" rel="stylesheet" />
+	</head>
+
+	<body>
+		
+	</body>
+</html>
