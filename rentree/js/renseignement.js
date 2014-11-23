3043d786a33d08f3c6946af71de0eb03942bead9
Artentica <riouallonvincent@gmail.com>
2014-11-24 00:07:05 +0100

    verif anniv

--- rentree/js/renseignement.js
+++ rentree/js/renseignement.js
@@ -22,9 +22,11 @@ function number(){
 
 function dateverifior(){
 	date_split = $('#birthday').val().split('/', 3);
-	console.log("date_split");
-	date = new Date(date_split[2], date_split[1] - 1, date_split[0]);
-	if ( date.getFullYear() == date_split[2] && (date.getMonth()+1)  == parseInt(date_split[1]) && date.getDate() == parseInt(date_split[0]) && /^(\d{1,2}\/){2}\d{4}$/.test(date_a_tester)) 
+	date = date_split[1]+"/"+date_split[0]+"/"+ date_split[2];
+	console.log(date);
+	dateV = new Date(date);
+	console.log(dateV);
+	if ( dateV.getFullYear() == date_split[2] && (dateV.getMonth()+1)  == parseInt(date_split[1]) && dateV.getDate() == date_split[0]) 
   		alert("La date est valide.");
 	else
   		alert("La date n'est pas valide.");
