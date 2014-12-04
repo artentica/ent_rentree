cbdf301847ba086e28ae19cb40d74ae217e07b4e
artentica <riouallonvincent@gmail.com>
2014-12-04 17:56:08 +0100

    reste 2pixel et la disparition à faire

--- rentree/views/documentseleves.html.php
+++ rentree/views/documentseleves.html.php
@@ -179,7 +179,7 @@ if( empty($_SESSION['identifiant'])) {
 
                     echo '<tr>
 						<td class="glyph2 active">'.$rang.'</td>
-						<td class="docname success">'.utf8_encode($value['libelle']).'</td>
+						<td class="docfield docname success">'.utf8_encode($value['libelle']).'</td>
 						<td class="tdglyph warning iconetable"><a target="_blank" href="pdf/'.utf8_encode($value['fichier']).'"><span class="glyphicon glyphicon-eye-open " aria-hidden="true"></span></a></td>
 						<td class="tdglyph info iconetable" ><a download href="pdf/'.utf8_encode($value['fichier']).'"><span class="glyphicon glyphicon-download" aria-hidden="true"></span></a></td>
 					</tr>';
@@ -195,7 +195,7 @@ if( empty($_SESSION['identifiant'])) {
                     $rang2=$rang+$value['rang'];
                     echo '<tr class="'.$value['promo'].' tabchange" style="display:none" value="'.$value['promo'].'">
 						<td class="active glyph2">'.$rang2.'</td>
-						<td class="docname success">'.utf8_encode($value['libelle']).'</td>
+						<td class="docfield docname success">'.utf8_encode($value['libelle']).'</td>
 						<td class="glyph tdglyph warning"><a target="_blank" href="pdf/'.utf8_encode($value['fichier']).'"><span class="glyphicon glyphicon-eye-open " aria-hidden="true"></span></a></td>
 						<td class="glyph tdglyph info" ><a download href="pdf/'.utf8_encode($value['fichier']).'"><span class="glyphicon glyphicon-download" aria-hidden="true"></span></a></td>
 					</tr>';
